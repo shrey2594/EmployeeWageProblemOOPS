46bd76c1e717c8d509c14e1b1a8d66cd57602578
shrey2594 <shrey2510@gmail.com>
2022-02-28 14:44:39 +0530

    Welcome to Employee Wage Problem

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace EmployeeWageProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            Check.checkPresentAbsent();
         }
     }
 }
