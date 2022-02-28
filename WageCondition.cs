bc97bd3dec3383e23ae044db0237de2a9b9762f0
shrey2594 <shrey2510@gmail.com>
2022-02-28 15:59:11 +0530

    Calculate wage according to given conditions

---
+++ WageCondition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblemOOPS
+{
+    public class WageCondition
+    {
+        public static bool WageCalculationCondition(int totalHour, int dayCount)
+        {
+            int workDayPerMonth = 20;
+            int hourCheck = 100;
+            if (totalHour <= hourCheck && dayCount <= workDayPerMonth)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
