4590351d01915163f1da1ef545a43e9286db724d
shrey2594 <shrey2510@gmail.com>
2022-02-28 14:35:42 +0530

    Welcome to Employee Wage Problem

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program.");
+            DailyWageComputation.CalculateDailyWage();
+        }
+    }
+}
