9f41962da292a24985812f04914407e771fe7b5b
shrey2594 <shrey2510@gmail.com>
2022-02-28 14:55:28 +0530

    Calculate daily wage of employee

---
+++ DailyWageComputation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblemOOPS
+{
+    public class DailyWageComputation
+    {
+        public static void CalculateDailyWage()
+        {
+            int wagePerHour = 20;
+            int fullDayHour = 8;
+            int moneyEarned;
+            if (Check.checkPresentAbsent() == true)
+            {
+                moneyEarned = wagePerHour * fullDayHour;
+                Console.WriteLine("The employee earned " + moneyEarned + " throughout the day.");
+            }
+            else
+            {
+                Console.WriteLine("The employee is Absent, hence earned nothing.");
+            }
+        }
+    }
+}
