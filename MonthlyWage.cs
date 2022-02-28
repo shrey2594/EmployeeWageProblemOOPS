113d17f57200672c54897ed89c780367d35df116
shrey2594 <shrey2510@gmail.com>
2022-02-28 15:22:44 +0530

    Compute monthly wage of employee

---
+++ MonthlyWage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblemOOPS
+{
+    public class MonthlyWage
+    {
+        public static void CalculateMonthlyWage(int workDayPerMonth, int wagePerHour, int fullDayHour, int partTimeHour)
+        {
+            int Total_Amount = 0;
+            for (int i = 0; i < workDayPerMonth; i++)
+            {
+                int value = Convert.ToInt32(Check.checkPresentAbsent());
+                if (value != 0)
+                { value += Convert.ToInt32(CheckPartTime.PartTime()); }
+                Total_Amount += SwitchCase.switchCase(value, wagePerHour, fullDayHour, partTimeHour);
+            }
+            Console.WriteLine("The employee got a salary of " + Total_Amount + " Rupees.");
+        }
+    }
+}
