9180b5daacfeaf11492bfaf6584dab2d8901f8dd
shrey2594 <shrey2510@gmail.com>
2022-02-28 15:17:11 +0530

    Computes daily wages of employee

--- DailyWageComputation.cs
+++ DailyWageComputation.cs
@@ -8,28 +8,15 @@ namespace EmployeeWageProblemOOPS
 {
     public class DailyWageComputation
     {
-            public static void CalculateDailyWage()
-            {
-                int wagePerHour = 20;
-                int fullDayHour = 8;
-                int partTimeHour = 4;
-                int moneyEarned;
-                if (Check.checkPresentAbsent() == true)
-                {
-                    if (CheckPartTime.PartTime() == true)
-                    {
-                        moneyEarned = wagePerHour * partTimeHour;
-                    }
-                    else
-                    {
-                        moneyEarned = wagePerHour * fullDayHour;
-                    }
-                    Console.WriteLine("The employee earned " + moneyEarned + " throughout the day.");
-                }
-                else
-                {
-                    Console.WriteLine("The employee is Absent, hence earned nothing.");
-                }
-            }
+        public static void CalculateDailyWage()
+        {
+            int wagePerHour = 20;
+            int fullDayHour = 8;
+            int partTimeHour = 4;
+            int final = Convert.ToInt32(Check.checkPresentAbsent());
+            if (final != 0)
+            { final += Convert.ToInt32(CheckPartTime.PartTime()); }
+            SwitchCase.switchCase(final, wagePerHour, fullDayHour, partTimeHour);
+        }
     }
 }
