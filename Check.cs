581d03ada3ceabe2251294c144c369a594823277
shrey2594 <shrey2510@gmail.com>
2022-02-28 14:45:31 +0530

    Checks Employee is Present or Absent

---
+++ Check.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblemOOPS
+{
+    public class Check
+    {
+        public static void checkPresentAbsent()
+        {
+            int isPresent = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == isPresent)
+            {
+                Console.WriteLine("Employee is presnt.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
+        }
+    }
+}
