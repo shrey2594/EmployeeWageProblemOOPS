f53c0fbfe821e4b71e0e3266f35967ca0b9c59f2
shrey2594 <shrey2510@gmail.com>
2022-02-28 15:58:18 +0530

    Compute monthly wage of employee

--- MonthlyWage.cs
+++ MonthlyWage.cs
@@ -11,12 +11,30 @@ namespace EmployeeWageProblemOOPS
         public static void CalculateMonthlyWage(int workDayPerMonth, int wagePerHour, int fullDayHour, int partTimeHour)
         {
             int Total_Amount = 0;
-            for (int i = 0; i < workDayPerMonth; i++)
+            int Total_Hour = 0;
+            for (int i = 1; i <= workDayPerMonth; i++)
             {
                 int value = Convert.ToInt32(Check.checkPresentAbsent());
                 if (value != 0)
-                { value += Convert.ToInt32(CheckPartTime.PartTime()); }
-                Total_Amount += SwitchCase.switchCase(value, wagePerHour, fullDayHour, partTimeHour);
+                {
+                    value += Convert.ToInt32(CheckPartTime.PartTime());
+                    if (value > 1)
+                    {
+                        Total_Hour += fullDayHour;
+                    }
+                    else
+                    {
+                        Total_Hour += partTimeHour;
+                    }
+                }
+                if (WageCondition.WageCalculationCondition(Total_Hour, i) == true)
+                {
+                    Total_Amount += SwitchCase.switchCase(value, wagePerHour, fullDayHour, partTimeHour);
+                }
+                else
+                {
+                    break;
+                }
             }
             Console.WriteLine("The employee got a salary of " + Total_Amount + " Rupees.");
         }
