9038b26b4a9aeeeda6c20135a3770db271d028ca
shrey2594 <shrey2510@gmail.com>
2022-02-28 15:07:53 +0530

    Checks Employee is part-time or full-time

---
+++ CheckPartTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblemOOPS
+{
+    public class CheckPartTime
+    {
+        public static bool PartTime()
+        {
+            int isPartTime = 1;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == isPartTime)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
